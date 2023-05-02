c387768d27261f7f632415abdff5cd994c6576c9
mrc <marcoemorales2015@gmail.com>
2023-05-02 18:24:34 -0300

    modulo 4 de open bootcamp

--- projectProducts/products/forms.py
+++ projectProducts/products/forms.py
@@ -1,7 +1,14 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Product
+
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ['title', 'description', 'stock']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'stock': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
--- projectProducts/products/views.py
+++ projectProducts/products/views.py
@@ -4,17 +4,19 @@ from .models import Product
 from .forms import ProductForm
 
 # Create your views here.
+
+
 def home(request):
-    return render(request, 'partials/table.html')
+    products = Product.objects.all().values()
+    return render(request, 'partials/table.html', {'products': products})
+
 
 def add(request):
     if request.method == 'POST':
-      form = ProductForm(request.POST)
-      if form.is_valid():
-        form.save()
-        return render(request, 'partials/form.html', {'form': form})
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'partials/form.html', {'form': form})
     else:
-      form = ProductForm()
-      return render(request, 'partials/form.html', {'form': form})
-    
-
+        form = ProductForm()
+        return render(request, 'partials/form.html', {'form': form})
