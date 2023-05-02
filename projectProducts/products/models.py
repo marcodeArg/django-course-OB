5914be1d9463b671d559637295b325121db15b82
mrc <marcoemorales2015@gmail.com>
2023-05-01 23:55:53 -0300

    modulo 4 de open bootcamp

--- projectProducts/products/models.py
+++ projectProducts/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.CharField(max_length=50, blank=False)
+    title = models.CharField(max_length=50, blank=False, )
     description = models.TextField(max_length=1000, blank=False)
     stock = models.IntegerField(blank=False, default=1)
 
